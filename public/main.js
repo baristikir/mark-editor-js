12a07c9d1da192e6a15e691f6a9352ae8ae8afce
Baris Tikir <brstkr@icloud.com>
2020-04-01 15:26:23 +0200

    Added Markdown Converter Functionality based on Showdown-JS-library

---
+++ public/main.js
@@ -0,0 +1,11 @@
+const textEditor = document.querySelector(".text-editor");
+const preview = document.querySelector(".preview");
+const converter = new showdown.Converter();
+
+textEditor.addEventListener("keyup", evt => {
+    const { value } = evt.target;
+
+    const html = converter.makeHtml(value)
+
+    preview.innerHTML = html;
+});
